444653fa3d68938d536be574a672602f3052b1ac
Anindya Bhattacharyya <bat020@gmail.com>
2016-02-04 21:31:20 +0000

    presentation cleaned up a bit

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 $(function () {
 
+  Handlebars.registerHelper('inPounds', pence => (pence / 100).toFixed(2));
+  Handlebars.registerHelper('hash', tags => tags.map(tag => '#' + tag).join(' '));
+
   var stockScript = $("#stock-template").html();
   var stockTemplate = Handlebars.compile(stockScript);
   $('.stock-list').append(stockTemplate(stock.list));
