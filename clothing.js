0e65ec9d0d2a34e69be128cb76be5ce63ad3cd9e
Anindya Bhattacharyya <bat020@gmail.com>
2016-02-05 05:13:26 +0000

    added comments to code

--- clothing.js
+++ clothing.js
@@ -1,3 +1,5 @@
+// business logic coded as methods of Items object
+
 function Item(uid, desc, taglist, price, quant) {
   this.uid = uid;
   this.desc = desc;
@@ -11,49 +13,55 @@ function Items(list) {
   this.list = list;
 }
 
-Items.prototype.matches = function (id, minQuant) {
-  return this.list.filter(item => item.uid === id && item.quant >= minQuant);
-}
+// basic list manipulation: takeOut, putIn, getItem
 
 Items.prototype.takeOut = function (id) {
-  this.matches(id, 1).forEach(item => item.quant--);
+  this._matches(id, 1).forEach(item => item.quant--);
 }
 
 Items.prototype.putIn = function (newItem) {
   if (newItem === null) return;
-  var match = this.matches(newItem.uid, 0);
+  var match = this._matches(newItem.uid, 0);
   match.forEach(item => item.quant++);
   if (match.length === 0) this.list.push(newItem);
 }
 
 Items.prototype.getItem = function (id) {
-  var match = this.matches(id, 1)
+  var match = this._matches(id, 1)
   if (match.length === 0) return null;
   return new Item(id, match[0].desc, match[0].taglist, match[0].price, 1);
 }
+
+Items.prototype._matches = function (id, minQuant) {
+  return this.list.filter(item => item.uid === id && item.quant >= minQuant);
+}
+
+// remove 0 quantity items from a list
 
 Items.prototype.stripZeros = function () {
   this.list = this.list.filter(item => item.quant > 0);
 }
 
+// money methods: rawTotal, voucher, netTotal
+
 Items.prototype.rawTotal = function () {
   return this.list.map(item => item.total()).reduce((x, y) => x + y, 0);
 }
 
-Items.prototype.allTags = function () {
-  return this.list.map(item => item.taglist).reduce((x, y) => x.concat(y), []);
-}
-
-Items.prototype.hasTag = function (tagToFind) {
-  return this.allTags().findIndex(tag => tag === tagToFind) >= 0;
-}
-
 Items.prototype.voucher = function () {
-  if (this.rawTotal() >= 7500 && this.hasTag("footwear")) return 1500;
+  if (this.rawTotal() >= 7500 && this._hasTag("footwear")) return 1500;
   if (this.rawTotal() >= 5000) return 1000;
   return 500;
 }
 
 Items.prototype.netTotal = function () {
   return this.rawTotal() - this.voucher();
+}
+
+Items.prototype._hasTag = function (tagToFind) {
+  return this._allTags().findIndex(tag => tag === tagToFind) >= 0;
+}
+
+Items.prototype._allTags = function () {
+  return this.list.map(item => item.taglist).reduce((x, y) => x.concat(y), []);
 }
--- main.js
+++ main.js
@@ -1,24 +1,45 @@
+// initialise the application
+
 $(function () {
-
-  var stock = buildStock();
+  var stock = buildStock(stockData);
   var trolley = new Items([]);
-
+  registerHelpers();
   generateHTML(stock, trolley);
-
 });
 
-function generateHTML(stock, trolley) {
+function buildStock(data) {
+  var itemList = data.map(hash =>
+    new Item(hash.uid, hash.desc, hash.taglist, hash.price, hash.quant)
+  );
+  return new Items(itemList);
+}
 
+function registerHelpers () {
   Handlebars.registerHelper('inPounds', pence => (pence / 100).toFixed(2));
+  Handlebars.registerHelper('justPounds', pence => (pence / 100).toFixed(0));
   Handlebars.registerHelper('inStock', q => q > 0 ? q + ' in stock' : 'out of stock');
   Handlebars.registerHelper('hash', tags => tags.map(tag => '#' + tag).join(' '));
   Handlebars.registerHelper('total', (p, q) => (p * q / 100).toFixed(2));
+}
 
+// generate HTML from templates
+
+function generateHTML(stock, trolley) {
+  generateStock(stock);
+  generateMessage(trolley);
+  generateTrolley(trolley);
+  addStockListeners(stock, trolley);
+  addTrolleyListeners(stock, trolley);
+}
+
+function generateStock(stock) {
   var stockScript = $("#stock-template").html();
   var stockTemplate = Handlebars.compile(stockScript);
   $('.stock-list').find('li').remove();
   $('.stock-list').append(stockTemplate(stock.list));
+}
 
+function generateMessage(trolley) {
   var messageScript = $("#message-template").html();
   var messageTemplate = Handlebars.compile(messageScript);
   var context = {
@@ -28,25 +49,32 @@ function generateHTML(stock, trolley) {
     net: trolley.netTotal()
   };
   $('.trolley-message').html(messageTemplate(context));
+}
 
+function generateTrolley(trolley) {
   var trolleyScript = $("#trolley-template").html();
   var trolleyTemplate = Handlebars.compile(trolleyScript);
   $('.trolley-list').find('li').remove();
   $('.trolley-list').append(trolleyTemplate(trolley.list));
+}
 
+// add listeners to buttons and links
+
+function addStockListeners(stock, trolley) {
   $('.stock-list').find('.buy-button').click(function () {
     var id = $(this).data('index');
     trolley.putIn(stock.getItem(id));
     stock.takeOut(id);
     generateHTML(stock, trolley);
-  })
+  });
+}
 
+function addTrolleyListeners(stock, trolley) {
   $('.trolley-list').find('.remove-link').click(function () {
     var id = $(this).data('index');
     stock.putIn(trolley.getItem(id));
     trolley.takeOut(id);
     trolley.stripZeros();
     generateHTML(stock, trolley);
-  })
-
+  });
 }
