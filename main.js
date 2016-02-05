72f438a7e7db27bfc383d6f13a9a4554225b1e4e
Anindya Bhattacharyya <bat020@gmail.com>
2016-02-05 04:09:44 +0000

    looks like it's working

--- main.js
+++ main.js
@@ -12,11 +12,22 @@ function generateHTML(stock, trolley) {
   Handlebars.registerHelper('inPounds', pence => (pence / 100).toFixed(2));
   Handlebars.registerHelper('inStock', q => q > 0 ? q + ' in stock' : 'out of stock');
   Handlebars.registerHelper('hash', tags => tags.map(tag => '#' + tag).join(' '));
+  Handlebars.registerHelper('total', (p, q) => (p * q / 100).toFixed(2));
 
   var stockScript = $("#stock-template").html();
   var stockTemplate = Handlebars.compile(stockScript);
   $('.stock-list').find('li').remove();
   $('.stock-list').append(stockTemplate(stock.list));
+
+  var messageScript = $("#message-template").html();
+  var messageTemplate = Handlebars.compile(messageScript);
+  var context = {
+    empty: trolley.rawTotal() === 0,
+    raw: trolley.rawTotal(),
+    voucher: trolley.voucher(),
+    net: trolley.netTotal()
+  };
+  $('.trolley-message').html(messageTemplate(context));
 
   var trolleyScript = $("#trolley-template").html();
   var trolleyTemplate = Handlebars.compile(trolleyScript);
@@ -27,6 +38,14 @@ function generateHTML(stock, trolley) {
     var id = $(this).data('index');
     trolley.putIn(stock.getItem(id));
     stock.takeOut(id);
+    generateHTML(stock, trolley);
+  })
+
+  $('.trolley-list').find('.remove-link').click(function () {
+    var id = $(this).data('index');
+    stock.putIn(trolley.getItem(id));
+    trolley.takeOut(id);
+    trolley.stripZeros();
     generateHTML(stock, trolley);
   })
 
